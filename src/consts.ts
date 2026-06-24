dbfc26c0d663eef8cdeb25cc3aa70b73ba04564a
俞昕炜 <simon.yxw@163.com>
2026-06-24 22:24:54 +0800

    customize dayu zhixing homepage

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '大愚知行录';
+export const SITE_SLOGAN = '知而不行，只是未知';
+export const SITE_DESCRIPTION = '记录 AI 时代的认知升级、数字化转型实践、第二大脑建设与商业实践。';
